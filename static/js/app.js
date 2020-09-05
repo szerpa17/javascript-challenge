95a0073dd23d6e17365ce10be1a33ad43c38b5d7
szerp <szerp002@gmail.com>
2020-09-04 23:17:44 -0400

    updated comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,16 @@
 // from data.js
 var tableData = data;
-// Create variable to select the html table body
+// Variable to select the html table body
 // This will be used to load the data onto the site
 var tbody = d3.select('tbody');
+// Variable for listener
+var button = d3.select('button');
 
 // Function that will render data into the site html table
 function renderData(data) {
     // For each object in the entered data variable
     data.forEach(rowData => {
-      // Append table row
+      // Append table row to designated html location
       var row = tbody.append('tr')
       // For each value in the object iteration 
       Object.values(rowData).forEach(val => {
@@ -20,5 +22,5 @@ function renderData(data) {
     });
   }
   
-// Render the table
+// Render the table values
 renderData(tableData);
